a94cf3767e7b477aaffc99939ada7e706b46b4b5
Marilin Rojas <rojasmarilin65@gmail.com>
2025-10-18 12:14:14 -0300

    Corrijo app.js y agrego configuración para Vercel

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-import express from "express";
-const app = express();
+// app.js
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const estudiantesRoutes = require("./routes/estudiantes");
 
+dotenv.config(); // Cargar variables de entorno
+connectDB(); // Conectar a la base de datos
+
+const app = express(); // Crear la aplicación de Express
+app.use(express.json()); // Middleware para leer JSON
+
+// Ruta base
 app.get("/", (req, res) => {
-  res.send("Servidor funcionando correctamente");
+  res.send("API de Estudiantes funcionando correctamente ✅");
 });
 
-export default app;
+// Rutas de estudiantes
+app.use("/api-estudiantes", estudiantesRoutes);
+
+// Iniciar el servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
